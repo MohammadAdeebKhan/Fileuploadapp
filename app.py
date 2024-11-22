8da8680b635b3eb50084a32feb7cc208a4c1e49f
MohammadAdeebKhan <147697159+MohammadAdeebKhan@users.noreply.github.com>
2024-11-22 16:00:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,40 @@ import os
 
 
 
+def chunk_audio(file_path, chunk_length_ms=10000):
+        """Chunk an audio file into smaller parts based on chunk length."""
+
+        #length = chunk_length_ms
+        audio = AudioSegment.from_file(file_path)
+        full_chunk = len(audio)
+        chunks = []
+        chunk_size = full_chunk // 4
+        start_ms = 0
+
+        
+        while start_ms < full_chunk:
+            
+            #print(f'This is sart_ms {start_ms}')
+            end_ms = start_ms + chunk_size
+            
+            #print(f"this is {end_ms}")
+            chunk_audio = audio[start_ms:end_ms]
+            
+            
+            chunk_file_path = f"chunk{start_ms}.mp3"
+            
+            if len(chunk_audio) > 1:
+                chunk_audio.export(chunk_file_path, format="mp3")
+                chunks.append(chunk_file_path)
+            
+            
+            #chunks.append(chunk_file_path)
+
+            
+            start_ms += chunk_size
+            
+        
+
 
 def process_audio(chunk):
 
@@ -61,39 +95,7 @@ def process_audio(chunk):
             print(full_transcription)
             return full_transcription , temp_file
 
-def chunk_audio(file_path, chunk_length_ms=10000):
-        """Chunk an audio file into smaller parts based on chunk length."""
 
-        #length = chunk_length_ms
-        audio = AudioSegment.from_file(file_path)
-        full_chunk = len(audio)
-        chunks = []
-        chunk_size = full_chunk // 4
-        start_ms = 0
-
-        
-        while start_ms < full_chunk:
-            
-            #print(f'This is sart_ms {start_ms}')
-            end_ms = start_ms + chunk_size
-            
-            #print(f"this is {end_ms}")
-            chunk_audio = audio[start_ms:end_ms]
-            
-            
-            chunk_file_path = f"chunk{start_ms}.mp3"
-            
-            if len(chunk_audio) > 1:
-                chunk_audio.export(chunk_file_path, format="mp3")
-                chunks.append(chunk_file_path)
-            
-            
-            #chunks.append(chunk_file_path)
-
-            
-            start_ms += chunk_size
-            
-        
         return chunks
 
 def agent(audio_file):
